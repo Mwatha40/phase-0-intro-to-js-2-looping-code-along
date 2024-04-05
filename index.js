9e345eb81f01b385b4b5def2793325976bd0a896
Allan <doshbank884@gmail.com>
2024-04-05 07:50:13 +0300

    phase-0-intro-to-js-2-looping-code-along complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+
+function writeCards(names,) {
+    let thankYouMessages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        let message = 'Thank you, ${names){i}}, for the wonderful ${eventName} surprise gift!';
+        thankYouMessages.push(message);
+
+    }
+    return thankYouMessages;
+}
+
+const names=["Guadalupe","Ollie","Aki"];
+const eventNames="birthday";
+const messages=writeCards(names,eventName);
+console.log(i);
+
+function countDown(number){
+    for (let i=number;i>=0;i--){
+        console.log(i);
+    }
+}
+
+countDown(10);
